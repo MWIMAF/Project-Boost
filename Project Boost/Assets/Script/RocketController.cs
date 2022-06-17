dba2498ac43e1bd9de91d10abc672046d1dc1757
MWIMAF <mwimaf@student.telkomuniversity.ac.id>
2022-06-17 22:17:22 +0700

    Adding Rocket Controller

---
+++ Project Boost/Assets/Script/RocketController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketController : MonoBehaviour
+{
+    Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NewMethod();
+    }
+
+    private void NewMethod()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddForce(Vector3.up);
+        }
+
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+        }
+    }
+}
